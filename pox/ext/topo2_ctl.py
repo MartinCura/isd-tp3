42c879c4a847141661e922150b772ac614a843b1
fran-cab <frenetic@ubuntu-1404.vagrantup.com>
2018-06-12 03:32:22 +0000

    componente similar a custom_spanning_tree.py que lanza los componentes necesarios para el controlador de la topologia2.

---
+++ pox/ext/topo2_ctl.py
@@ -0,0 +1,39 @@
+# Copyright 2012 James McCauley
+#
+# This file is part of POX.
+#
+# POX is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# POX is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with POX.  If not, see <http://www.gnu.org/licenses/>.
+
+
+def launch():
+    import pox.log.color
+    pox.log.color.launch()
+    import pox.log
+    pox.log.launch(format="[@@@bold@@@level%(name)-22s@@@reset] " +
+                        "@@@bold%(message)s@@@normal")
+    from pox.core import core
+  
+    import pox.openflow.discovery
+    pox.openflow.discovery.launch()
+
+    core.getLogger("openflow.spanning_tree").setLevel("DEBUG")
+  
+    import custom_l2_learning
+    custom_l2_learning.launch()
+
+    # import pox.openflow.spanning_tree
+    # pox.openflow.spanning_tree.launch()
+
+    import custom_firewall
+    custom_firewall.launch()
